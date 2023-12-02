9672fc6b5e74f56485779c15587d6ceb51c91622
Daniel Kempner <djkempner@gmail.com>
2023-12-02 08:18:02 -0800

    day02

--- day02/2.ts
+++ day02/2.ts
@@ -1,7 +1,47 @@
 import { solve, bitsToInt } from './helper.ts'
 
+const colors = ['blue', 'red', 'green']
+type Colors = typeof colors[number]
+
+type Game = { [key: Colors]: number }
+
+// 25:00
+
 solve((lines) => {
+  let sum = 0
   lines.forEach((line) => {
-    console.log(line)
+    const game = getGame(line)
+    sum = sum + game.blue * game.red * game.green
   })
+  console.log(sum)
 })
+
+function getGame(line: string) {
+  const max: Game = {
+    blue: 0,
+    red: 0,
+    green: 0,
+  }
+
+  const rounds = getRounds(removeGameId(line))
+
+  rounds.forEach((round) => {
+    const roundParts = round.split(',').map((x) => x.trim())
+    roundParts.forEach((roundPart) => {
+      const color = colors.find((x) => roundPart.includes(x))
+      if (!color) throw new Error('')
+      const number = Number(roundPart.replace(` ${color}`, ''))
+      if (number > max[color]) max[color] = number
+    })
+  })
+
+  return max
+}
+
+function removeGameId(line: string) {
+  return line.split(': ').at(1)!
+}
+
+function getRounds(line: string) {
+  return line.split(';').map((x) => x.trim())
+}
