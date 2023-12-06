75d448d8ce3c131d644b9ad620709a97ddf09e80
Daniel Kempner <djkempner@gmail.com>
2023-12-05 21:16:51 -0800

    day06 pt2

--- day06/2.ts
+++ day06/2.ts
@@ -1,7 +1,40 @@
 import { solve, bitsToInt } from './helper.ts'
 
+const distanceMap = new Map<number, number>()
+
 solve((lines) => {
-  lines.forEach((line) => {
-    console.log(line)
-  })
+  let options: number[] = []
+  const [timesRaw, distancesRaw] = lines
+  const times = timesRaw
+    .split('Time:')[1]
+    .trim()
+    .split(' ')
+    .filter((x) => x)
+    .map(Number)
+  const distances = distancesRaw
+    .split('Distance:')[1]
+    .trim()
+    .split(' ')
+    .filter((x) => x)
+    .map(Number)
+
+  const time = Number(times.map((x) => x.toString()).join(''))
+  const distance = Number(distances.map((x) => x.toString()).join(''))
+
+  let localOptions = 0
+
+  for (let j = 0; j < time; j++) {
+    if (calcDistance(j, time) > distance) {
+      localOptions++
+    }
+  }
+
+  options.push(localOptions)
+
+  return options.reduce((prev, curr) => prev * curr, 1)
 })
+
+function calcDistance(hold: number, total: number) {
+  const timeToSwim = total - hold
+  return timeToSwim * hold
+}
